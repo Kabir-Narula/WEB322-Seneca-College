2cfaba990e7a1777e806494fbdb70b2fcdfd2f5f
Pupuu <137004973+Kim-Chaewonn@users.noreply.github.com>
2023-11-26 19:04:09 -0500

    Update a1.js

--- A1/a1.js
+++ A1/a1.js
@@ -4,7 +4,7 @@
  * No part of this assignment has been copied manually or electronically from any other source
  * (including web sites) or distributed to other students.
  *
- * Name: Kabir Narula Student ID: 127962223 Date: 8 September 2023
+ * 
  *
  ********************************************************************************/
 
